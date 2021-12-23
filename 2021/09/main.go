1ca5bab25f1ab4d01474d2238afa23dd2d405214
Luis Sanchez <lcsanchez08@gmail.com>
2021-12-23 09:51:56 -0600

    Day 09 solutions

--- 2021/09/main.go
+++ 2021/09/main.go
@@ -4,22 +4,62 @@ import (
 	"bufio"
 	"bytes"
 	_ "embed"
+	"fmt"
 	"io"
 	"log"
+	"sort"
 	"strconv"
 )
 
 //go:embed testdata/input.txt
 var input []byte
 
+type Cave [][]int
+
+func (c Cave) Get(p *Point) (int, bool) {
+	if len(c) == 0 {
+		return 0, false
+	}
+
+	isOutOfBounds := p.X < 0 || p.X >= len(c[0]) || p.Y < 0 || p.Y >= len(c)
+	if isOutOfBounds {
+		return 0, false
+	}
+
+	return c[p.Y][p.X], true
+}
+
+type Point struct {
+	X, Y int
+}
+
+func (p *Point) Add(o *Point) *Point {
+	return &Point{X: p.X + o.X, Y: p.Y + o.Y}
+}
+
+func (p *Point) HashCode() string {
+	return fmt.Sprintf("%d,%d", p.X, p.Y)
+}
+
+var (
+	TOP             = &Point{Y: -1}
+	BOTTOM          = &Point{Y: 1}
+	LEFT            = &Point{X: -1}
+	RIGHT           = &Point{X: 1}
+	ADJACENT_POINTS = []*Point{TOP, BOTTOM, LEFT, RIGHT}
+)
+
 func main() {
-	_, err := readInput(bytes.NewReader(input))
+	cave, err := readInput(bytes.NewReader(input))
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println(findRiskLevelSum(cave))
+	fmt.Println(findThreeLargestBasinMultiple(cave))
 }
 
-func readInput(r io.Reader) ([][]int, error) {
+func readInput(r io.Reader) (Cave, error) {
 	scanner := bufio.NewScanner(r)
 	cave := [][]int{}
 
@@ -41,4 +81,75 @@ func readInput(r io.Reader) ([][]int, error) {
 	}
 
 	return cave, nil
+}
+
+func findRiskLevelSum(cave Cave) int {
+	sum := 0
+	for rowIdx, row := range cave {
+		for colIdx, num := range row {
+			if isLowPoint(cave, &Point{X: colIdx, Y: rowIdx}) {
+				sum += num + 1
+			}
+		}
+	}
+
+	return sum
+}
+
+func isLowPoint(cave Cave, position *Point) bool {
+	currentValue, _ := cave.Get(position)
+
+	for _, p := range ADJACENT_POINTS {
+		value, ok := cave.Get(position.Add(p))
+		if !ok {
+			continue
+		}
+
+		if value <= currentValue {
+			return false
+		}
+	}
+
+	return true
+}
+
+func findThreeLargestBasinMultiple(cave Cave) int {
+	basinSizes := []int{}
+	for rowIdx, row := range cave {
+		for colIdx, _ := range row {
+			current := &Point{X: colIdx, Y: rowIdx}
+			if isLowPoint(cave, current) {
+				basin := map[string]*Point{current.HashCode(): current}
+				findBasinSize(cave, current, basin)
+
+				basinSizes = append(basinSizes, len(basin))
+			}
+		}
+	}
+
+	sort.Ints(basinSizes)
+	basinSizes = basinSizes[len(basinSizes)-3:]
+
+	return basinSizes[0] * basinSizes[1] * basinSizes[2]
+}
+
+func findBasinSize(cave Cave, position *Point, basin map[string]*Point) {
+	currentValue, _ := cave.Get(position)
+
+	for _, p := range ADJACENT_POINTS {
+		posToCheck := position.Add(p)
+		if _, ok := basin[posToCheck.HashCode()]; ok {
+			continue
+		}
+
+		value, ok := cave.Get(posToCheck)
+		if !ok || value == 9 {
+			continue
+		}
+
+		if value > currentValue {
+			basin[posToCheck.HashCode()] = posToCheck
+			findBasinSize(cave, posToCheck, basin)
+		}
+	}
 }
--- 2021/09/main_test.go
+++ 2021/09/main_test.go
@@ -10,7 +10,7 @@ import (
 
 //go:embed testdata/test-input.txt
 var testInput []byte
-var expectedCave = [][]int{
+var expectedCave = Cave{
 	{2, 1, 9, 9, 9, 4, 3, 2, 1, 0},
 	{3, 9, 8, 7, 8, 9, 4, 9, 2, 1},
 	{9, 8, 5, 6, 7, 8, 9, 8, 9, 2},
@@ -22,4 +22,54 @@ func TestReadInput(t *testing.T) {
 	cave, err := readInput(bytes.NewReader(testInput))
 	require.NoError(t, err)
 	require.Equal(t, expectedCave, cave)
+}
+
+func TestIsLowPoint(t *testing.T) {
+	type test struct {
+		position *Point
+		expected bool
+	}
+
+	tests := []test{
+		{position: &Point{X: 0, Y: 0}, expected: false},
+		{position: &Point{X: 1, Y: 0}, expected: true},
+		{position: &Point{X: 1, Y: 1}, expected: false},
+		{position: &Point{X: 9, Y: 0}, expected: true},
+		{position: &Point{X: 6, Y: 4}, expected: true},
+	}
+
+	for _, tc := range tests {
+		got := isLowPoint(expectedCave, tc.position)
+		require.Equal(t, tc.expected, got)
+	}
+}
+
+func TestFindRiskLevelSum(t *testing.T) {
+	sum := findRiskLevelSum(expectedCave)
+	require.Equal(t, 15, sum)
+}
+
+func TestFindThreeLargestBasinMultiple(t *testing.T) {
+	m := findThreeLargestBasinMultiple(expectedCave)
+	require.Equal(t, 1134, m)
+}
+
+func TestFindBasinSize(t *testing.T) {
+	type test struct {
+		position *Point
+		expected int
+	}
+
+	tests := []test{
+		{position: &Point{X: 1, Y: 0}, expected: 3},
+		{position: &Point{X: 9, Y: 0}, expected: 9},
+		{position: &Point{X: 2, Y: 2}, expected: 14},
+		{position: &Point{X: 6, Y: 4}, expected: 9},
+	}
+
+	for _, tc := range tests {
+		basin := map[string]*Point{tc.position.HashCode(): tc.position}
+		findBasinSize(expectedCave, tc.position, basin)
+		require.Equal(t, tc.expected, len(basin))
+	}
 }
