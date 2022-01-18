f48de003b6c9215609fe567928e2c529950889bf
Luis Sanchez <lcsanchez08@gmail.com>
2022-01-18 08:34:48 -0600

    Day 17 solutions

--- 2021/17/main.go
+++ 2021/17/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -25,10 +26,13 @@ type Input struct {
 }
 
 func main() {
-	_, err := readInput(bytes.NewReader(input))
+	input, err := readInput(bytes.NewReader(input))
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println(calculateSummation(input.Y.First - 1))
+	fmt.Println(countStartingVelocities(input))
 }
 
 func readInput(r io.Reader) (*Input, error) {
@@ -72,15 +76,96 @@ func splitTuple(s string) (*Tuple, error) {
 		return nil, err
 	}
 
-	if first > second {
-		return &Tuple{
-			First:  second,
-			Second: first,
-		}, nil
-	}
-
 	return &Tuple{
 		First:  first,
 		Second: second,
 	}, nil
+}
+
+func findSummationInRange(t *Tuple) (int, int) {
+	sum := 0
+	idx := 1
+	for {
+		sum += idx
+
+		if sum > t.First {
+			break
+		}
+
+		idx++
+	}
+
+	return idx, sum
+}
+
+func calculateSummation(n int) int {
+	sum := 0
+	for i := 0; i <= n; i++ {
+		sum += i
+	}
+
+	return sum
+}
+
+func countStartingVelocities(input *Input) int {
+	velocities := (int(math.Abs(float64(input.X.Second-input.X.First))) + 1) * (int(math.Abs(float64(input.Y.Second-input.Y.First))) + 1)
+
+	xCurrent := (input.X.Second / 2) + (input.X.Second % 2)
+	for i := xCurrent; i > 0; i-- {
+		sum := i
+		steps := 1
+		stepMin := -1
+		var stepMax int
+		for {
+			if input.X.Second < sum {
+				stepMax = steps - 1
+				break
+			}
+
+			if stepMin == -1 && input.X.First <= sum {
+				stepMin = steps
+			}
+
+			if i == steps {
+				if input.X.First < sum && sum < input.X.Second {
+					stepMax = math.MaxInt64
+				}
+
+				break
+			}
+
+			sum += i - steps
+			steps++
+
+		}
+
+		if stepMin != -1 {
+			velocities += calculateYVelocitiesInSteps(input.Y, stepMin, stepMax)
+		}
+
+	}
+
+	return velocities
+}
+
+// These could be precalculated instead of for each different x
+func calculateYVelocitiesInSteps(yRange *Tuple, stepMin int, stepMax int) int {
+	yVelocities := 0
+	for i := int(math.Abs(float64(yRange.First))) - 1; i >= yRange.First; i-- {
+		currentY := i
+		for step := 1; step <= stepMax; step++ {
+			if currentY < yRange.First {
+				break
+			}
+
+			if step >= stepMin && currentY >= yRange.First && currentY <= yRange.Second {
+				yVelocities++
+				break
+			}
+
+			currentY += i - step
+		}
+	}
+
+	return yVelocities
 }
--- 2021/17/main_test.go
+++ 2021/17/main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	_ "embed"
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -24,4 +25,28 @@ func TestReadInput(t *testing.T) {
 			Second: -10,
 		},
 	}, input)
+}
+
+func TestFindSummationInRange(t *testing.T) {
+	i, sum := findSummationInRange(&Tuple{First: 20, Second: 30})
+	require.Equal(t, 6, i)
+	require.Equal(t, 21, sum)
+}
+
+func TestCalculateSummation(t *testing.T) {
+	sum := calculateSummation(9)
+	require.Equal(t, 45, sum)
+}
+
+func TestCountStartingVelocities(t *testing.T) {
+	input, err := readInput(bytes.NewReader(testInput))
+	require.NoError(t, err)
+	require.Equal(t, 112, countStartingVelocities(input))
+}
+
+func TestCalculateYVelocities(t *testing.T) {
+	input, err := readInput(bytes.NewReader(testInput))
+	require.NoError(t, err)
+	require.Equal(t, 3, calculateYVelocitiesInSteps(input.Y, 2, 2))
+	require.Equal(t, 3, calculateYVelocitiesInSteps(input.Y, 4, math.MaxInt64))
 }
