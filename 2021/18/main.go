a69e867156d9f06b1eb6ea7517c6ee98e294bd58
Luis Sanchez <lcsanchez08@gmail.com>
2022-01-19 22:10:57 -0600

    Day 18 solutions

--- 2021/18/main.go
+++ 2021/18/main.go
@@ -4,9 +4,11 @@ import (
 	"bufio"
 	"bytes"
 	_ "embed"
+	"fmt"
 	"io"
 	"log"
 	"strconv"
+	"strings"
 )
 
 //go:embed testdata/input.txt
@@ -18,11 +20,58 @@ type Node struct {
 	data  int
 }
 
+func (n *Node) clone() *Node {
+	new := &Node{
+		data: n.data,
+	}
+
+	if n.left != nil {
+		new.left = n.left.clone()
+	}
+
+	if n.right != nil {
+		new.right = n.right.clone()
+	}
+
+	return new
+}
+
+func (n *Node) isData() bool {
+	if n == nil {
+		return false
+	}
+
+	return n.left == nil && n.right == nil
+}
+
 func main() {
-	_, err := readInput(bytes.NewReader(input))
+	input, err := readInput(bytes.NewReader(input))
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	total := input[0]
+	for i := 1; i < len(input); i++ {
+		total = reduce(sum(total.clone(), input[i].clone()))
+	}
+
+	fmt.Println(magnitude(total))
+
+	var m int
+	for i := 0; i < len(input); i++ {
+		for j := 0; j < len(input); j++ {
+			if i == j {
+				continue
+			}
+
+			current := magnitude(reduce(sum(input[i].clone(), input[j].clone())))
+			if current > m {
+				m = current
+			}
+		}
+	}
+
+	fmt.Println(m)
 }
 
 func readInput(r io.Reader) ([]*Node, error) {
@@ -43,6 +92,34 @@ func readInput(r io.Reader) ([]*Node, error) {
 	}
 
 	return nodes, nil
+}
+
+func magnitude(node *Node) int {
+	if node.isData() {
+		return node.data
+	}
+
+	return 3*magnitude(node.left) + 2*magnitude(node.right)
+}
+
+func print(node *Node) string {
+	var sb strings.Builder
+	printHelper(node, &sb)
+
+	return sb.String()
+}
+
+func printHelper(node *Node, sb *strings.Builder) {
+	if node.isData() {
+		sb.WriteString(strconv.Itoa(node.data))
+		return
+	}
+
+	sb.WriteRune('[')
+	printHelper(node.left, sb)
+	sb.WriteRune(',')
+	printHelper(node.right, sb)
+	sb.WriteRune(']')
 }
 
 func readNode(s string) (*Node, error) {
@@ -93,4 +170,140 @@ func splitOnFreeComma(s string) (string, string) {
 	}
 
 	return "", ""
+}
+
+func sum(left *Node, right *Node) *Node {
+	return &Node{
+		left:  left,
+		right: right,
+	}
+}
+
+func reduce(root *Node) *Node {
+	for {
+		for {
+			exploder := findExploder(root, 0)
+			if exploder == nil {
+				break
+			}
+
+			explode(root, exploder)
+		}
+
+		splitter := findSplitter(root)
+		if splitter == nil {
+			break
+		}
+		split(splitter)
+	}
+
+	return root
+}
+
+func findSplitter(node *Node) *Node {
+	if node.isData() {
+		if node.data > 9 {
+			return node
+		}
+
+		return nil
+	}
+
+	splitter := findSplitter(node.left)
+	if splitter != nil {
+		return splitter
+	}
+
+	splitter = findSplitter(node.right)
+	if splitter != nil {
+		return splitter
+	}
+
+	return nil
+}
+
+func split(node *Node) {
+	node.left = &Node{data: node.data / 2}
+	node.right = &Node{data: node.data/2 + (node.data % 2)}
+	node.data = 0
+}
+
+func explode(root *Node, exploder *Node) {
+	left := findLeft(root, exploder)
+	if left != nil {
+		left.data += exploder.left.data
+	}
+
+	right := findRight(root, exploder)
+	if right != nil {
+		right.data += exploder.right.data
+	}
+
+	exploder.left = nil
+	exploder.right = nil
+	exploder.data = 0
+}
+
+func findExploder(node *Node, depth int) *Node {
+	if node.isData() {
+		return nil
+	}
+
+	if node.left.isData() && node.right.isData() {
+		if depth < 4 {
+			return nil
+		}
+
+		return node
+	}
+
+	exploder := findExploder(node.left, depth+1)
+	if exploder != nil {
+		return exploder
+	}
+	exploder = findExploder(node.right, depth+1)
+
+	return exploder
+}
+
+func findLeft(root *Node, target *Node) *Node {
+	var result *Node
+	findLeftHelper(root, target, &result)
+
+	return result
+}
+
+func findLeftHelper(node *Node, target *Node, result **Node) bool {
+	if node == target {
+		return true
+	}
+
+	if node.isData() {
+		*result = node
+		return false
+	}
+
+	return findLeftHelper(node.left, target, result) ||
+		findLeftHelper(node.right, target, result)
+}
+
+func findRight(root *Node, target *Node) *Node {
+	var result *Node
+	findRightHelper(root, target, &result)
+
+	return result
+}
+
+func findRightHelper(node *Node, target *Node, result **Node) bool {
+	if node == target {
+		return true
+	}
+
+	if node.isData() {
+		*result = node
+		return false
+	}
+
+	return findRightHelper(node.right, target, result) ||
+		findRightHelper(node.left, target, result)
 }
--- 2021/18/main_test.go
+++ 2021/18/main_test.go
@@ -43,3 +43,24 @@ func TestReadInput(t *testing.T) {
 		right: &Node{data: 1},
 	}, input[1])
 }
+
+func TestFindExploder(t *testing.T) {
+	input, err := readInput(bytes.NewReader(testInput))
+	require.NoError(t, err)
+
+	root := &Node{
+		left:  input[0],
+		right: input[1],
+	}
+
+	exploder := findExploder(root, 0)
+	require.Equal(t, &Node{left: &Node{data: 4}, right: &Node{data: 3}}, exploder)
+
+	left := findLeft(root, exploder)
+	require.Nil(t, left)
+
+	right := findRight(root, exploder)
+	require.Equal(t, &Node{data: 4}, right)
+
+	require.Equal(t, root, root.clone())
+}
